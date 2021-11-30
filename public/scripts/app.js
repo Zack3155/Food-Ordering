c3b1fc30636692a21e65e5ca31327ee583090ade
Alex Thon <ghaiquol@gmail.com>
2021-11-30 16:30:12 +0000

    Add content to app.js

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,4 +1,21 @@
 // Client facing scripts here
+// eslint-disable-next-line no-undef
+
+const createNavBar = () => {
+  return (
+    `<nav class="navbar">
+      <div class="logo">logo here </div>
+      <ul>
+        <li><a></a>Home</li>
+        <li><a></a>Order</li>
+        <li><a></a>Contact</li>
+      </ul>
+    </nav>`
+  );
+};
+
+// eslint-disable-next-line no-undef
 $(document).ready(function() {
-  
-}):
+  // eslint-disable-next-line no-undef
+  $('.menu').append(createNavBar);
+});
