98906f85c1ec0facc847357e36ace465b96a4ebf
Zack <zhanbin3155@gmail.com>
2021-12-01 13:31:32 -0500

    Implement some Cart Page Logic

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -60,11 +60,44 @@ const dishes = [{ a: 1 }, { b: 2 }, { c: 3 }, { d: 4 }, { e: 5 }, { f: 6 }, { g:
 const removeDish = function (dish_name, dishes) {
   return dishes.filter(itm => !itm[dish_name]);
 };
-///////////////////////////////////////////////////////////////////////////////////////////////
+
 
 // Cart Page Logic Implementation
 
-// Show cart content according to Dishes array
+// Create single cart item
+const createCartItem = function (name, pic_url, intro, quantity) {
+  return (`
+  <div class="dish-container">
+        <header>
+          <p class="name">${name}</p>
+          <img
+            src="${pic_url}">
+        </header>
+        <div class="content">
+          <div class="remove"><button><i class="far fa-trash-alt"></i></button></div>
+          <article>
+            <p>${intro}</p>
+          </article>
+          <footer>
+            <p>Please Edit the Quantity</p>
+            <div class="quantity">
+              <button class="minus"><i class="fas fa-angle-double-left"></i></button>
+              <div class="quantity-input"><output>${quantity}</output></div>
+              <button class="add"><i class="fas fa-angle-double-right"></i></button>
+            </div>
+          </footer>
+        </div>
+      </div>
+  `);
+};
+
+// Dynamically show cart content according to Dishes array
+const renderCart = function (dishes) {
+  const cartContainer = $('.dishes');
+  const $itm = createCartItem('Photosimysia', 'https://loveincorporated.blob.core.windows.net/contentimages/gallery/88f787f4-40c9-4084-92f1-5df6dc94fb72-french-onion-soup.jpg', `Very few dishes are as comforting as French onion soup – a blend of mellow, slowly cooked, caramelised onions in a broth laced with white wine and cognac. It's thought that a version of the soup has existed since at least Roman times, but the modern version originated in 18th-century Paris. The soup is served in a ramekin, topped with a slice of baguette and cheese that's then melted under a grill.`, '1');
+  cartContainer.append($itm);
+}
+///////////////////////////////////////////////////////////////////////////////////////////////
 
 
 // eslint-disable-next-line no-undef
@@ -78,11 +111,11 @@ $(document).ready(function () {
   ///////////////////////////////////////////////////////////////////////////////////////////////
   // Cart Page Implementation
 
+  renderCart(dishes);
 
   // Remove Button
-  $(".remove button").click(function () {
-
-
+  $(".remove button").click(function (event) {
+    event.preventDefault();
     // Remove dish from Dishes array
     //removeDish(dish_name, dishes);
 
@@ -92,27 +125,34 @@ $(document).ready(function () {
   });
 
   // Add Button
-  $(".add").click(function () {
-    const input = $('.quantity-input output');
+  $(".add").click(function (event) {
+    event.preventDefault();
+    const output = $(this).closest(".dish-container").find('output');
     const MAX = 9;
-    let quantity = Number(input.text());
+    let quantity = Number(output.text());
     if (quantity < MAX) {
-      input.text(++quantity);
+      output.text(++quantity);
+      // set quantity data
+
     }
   });
 
   // Minus Button
-  $(".minus").click(function () {
-    const input = $('.quantity-input output');
+  $(".minus").click(function (event) {
+    event.preventDefault();
+    const output = $(this).closest(".dish-container").find('output');
     const Min = 1;
-    let quantity = Number(input.text());
+    let quantity = Number(output.text());
     if (quantity > Min) {
-      input.text(--quantity);
+      output.text(--quantity);
+      // set quantity data
+
     }
   });
 
   // Checkout Button
-  $(".checkout button").click(function () {
+  $(".checkout button").click(function (event) {
+    event.preventDefault();
     window.location.href = "/checkout";
   });
   ///////////////////////////////////////////////////////////////////////////////////////////////
