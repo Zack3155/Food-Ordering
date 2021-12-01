120fa56da580beedab6d16cc3a5fdfc0cf82c4b3
Zack <zhanbin3155@gmail.com>
2021-11-30 19:05:53 -0500

    Setup Files for Cart Page

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,4 +1,29 @@
 // Client facing scripts here
-$(document).ready(function () {
+// eslint-disable-next-line no-undef
 
+const createNavBar = () => {
+  return (
+    `<nav class="navbar">
+      <div class="logo">
+         <img alt="logo" src="../logo/logo.svg">
+      </div>
+      <ul>
+        <li><a></a>Home</li>
+        <li><a></a>Order</li>
+        <li><a></a>Contact</li>
+      </ul>
+      <div class="cart">
+         <img alt="logo" src="../images/cart.svg">
+      </div>
+
+
+    </nav>`
+  );
+};
+
+// eslint-disable-next-line no-undef
+$(document).ready(function () {
+  // eslint-disable-next-line no-undef
+  $('.menu').append(createNavBar);
+  console.log('DOM is Ready');
 });
