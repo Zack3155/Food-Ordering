7239afd9d9b315e10903f6693d58940cc2a1bbd9
Alex Thon <ghaiquol@gmail.com>
2021-12-02 05:59:04 +0000

    Add menu items and footer

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -9,7 +9,7 @@ const createNavBar = () => {
       </div>
       <ul class="menu-item-container">
         <li><a href="/home">Home</a></li>
-        <li><a href="/menu">Menu</a></li>
+        <li><a href="#menu">Menu</a></li>
         <li><a href="/contact">Contact</a></li>
       </ul>
       <div class="cart">
@@ -50,6 +50,69 @@ const blueBannerContent = () => {
 };
 
 
+
+// create menu item with its description
+const createMenuItem = (menuObj) => {
+  return (`
+    <article class="item-container">
+      <div class="dish-pic"> <img alt="picture of dish" src=${menuObj.image}> </div>
+      <section class="component-container"> 
+        <h3 class="title"> ${menuObj.title} </h3> 
+        <p> ${menuObj.desc} </p> 
+        
+        <button type="button" 
+          class="btn btn-default btn-sm bg-to-red">
+          <span class="glyphicon 
+              glyphicon-shopping-cart">
+          </span>
+          <b> Add to Cart </b>
+        </button>
+      </section>
+    </article>
+   `);
+};
+
+
+const footer = () => {
+  return (
+    `<div class="footer-dark bg-to-light">
+        <footer>
+             <div class="container">
+                <div class="row">
+                    <div class="col-sm-6 col-md-3 item">
+                        <h3>Services</h3>
+                        <ul>
+                            <li><a href="#">Home deliveries</a></li>
+                            <li><a href="#">In restaurant services</a></li>
+                            <li><a href="#">Cook for events</a></li>
+                        </ul>
+                    </div>
+                    <div class="col-sm-6 col-md-3 item">
+                        <h3>About</h3>
+                        <ul>
+                            <li><a href="#">Fresh restaurant</a></li>
+                            <li><a href="#">jobs</a></li>
+                        </ul>
+                    </div>
+                    <div class="col-md-6 item text">
+                        <h3>Fresh restaurant</h3>
+                        <p>Praesent sed lobortis mi. Suspendisse vel placerat ligula. Vivamus ac sem lacus. Ut vehicula rhoncus elementum. Etiam quis tristique lectus. Aliquam in arcu eget velit pulvinar dictum vel in justo.</p>
+                    </div>
+                    <div class="col item social"><a href="#"><i class="icon ion-social-facebook"></i></a><a href="#"><i class="icon ion-social-twitter"></i></a><a href="#"><i class="icon ion-social-snapchat"></i></a><a href="#"><i class="icon ion-social-instagram"></i></a></div>
+                </div>
+                <p class="copyright">Fresh © 2021</p>
+             </div>
+        </footer>
+    </div> 
+    `);
+
+
+
+
+
+};
+
+
 ///////////////////////////////////////////////////////////////////////////////////////////////
 // Client Side Logic Implementation
 
@@ -58,11 +121,11 @@ const dishes = [{ a: 1 }, { b: 2 }, { c: 3 }, { d: 4 }, { e: 5 }, { f: 6 }, { g:
 
 // Remove dish from Dishes given by its name
 // Return modified Dishes
-const removeDish = function (dish_name, dishes) {
+const removeDish = function(dish_name, dishes) {
   return dishes.filter(itm => !itm[dish_name]);
 };
 
-const setDishQuantity = function (dish_name, quantity, dishes) {
+const setDishQuantity = function(dish_name, quantity, dishes) {
   for (const itm of dishes) {
     if (itm[dish_name])
       itm[dish_name] = quantity;
@@ -76,7 +139,7 @@ const setDishQuantity = function (dish_name, quantity, dishes) {
 
 // Cart Page Logic Implementation
 // Create single cart item
-const createCartItem = function (name, pic_url, intro, quantity) {
+const createCartItem = function(name, pic_url, intro, quantity) {
   return (`
   <div class="dish-container">
         <header>
@@ -103,12 +166,12 @@ const createCartItem = function (name, pic_url, intro, quantity) {
 };
 
 // Dynamically show cart content according to Dishes array
-const renderCart = function (dishes) {
+const renderCart = function(dishes) {
   const itemsIncart = [];
-  const name = 'Photosimysia'
-  const pic_url = 'https://loveincorporated.blob.core.windows.net/contentimages/gallery/88f787f4-40c9-4084-92f1-5df6dc94fb72-french-onion-soup.jpg'
-  const intro = `Very few dishes are as comforting as French onion soup – a blend of mellow, slowly cooked, caramelised onions in a broth laced with white wine and cognac. It's thought that a version of the soup has existed since at least Roman times, but the modern version originated in 18th-century Paris. The soup is served in a ramekin, topped with a slice of baguette and cheese that's then melted under a grill.`
-  const quantity = '1'
+  const name = 'Photosimysia';
+  const pic_url = 'https://loveincorporated.blob.core.windows.net/contentimages/gallery/88f787f4-40c9-4084-92f1-5df6dc94fb72-french-onion-soup.jpg';
+  const intro = `Very few dishes are as comforting as French onion soup – a blend of mellow, slowly cooked, caramelised onions in a broth laced with white wine and cognac. It's thought that a version of the soup has existed since at least Roman times, but the modern version originated in 18th-century Paris. The soup is served in a ramekin, topped with a slice of baguette and cheese that's then melted under a grill.`;
+  const quantity = '1';
   const cartContainer = $('.dishes');
   const $itm = createCartItem(name, pic_url, intro, quantity);
   
@@ -122,13 +185,43 @@ const renderCart = function (dishes) {
 ///////////////////////////////////////////////////////////////////////////////////////////////
 
 
-// eslint-disable-next-line no-undef
-$(document).ready(function () {
-  // eslint-disable-next-line no-undef
+/*
+  DUMMY MENU OBJECTS
+*/
+
+const renderHomePage = (db) => {
   $('.menu').append(createNavBar);
   $('.search-container').append(searchButton);
   $('.shape-decoration').append(createDecoration);
   $('.blue-container').append(blueBannerContent);
+  $('.footer-container').append(footer);
+  for (let dish of db) {
+    // eslint-disable-next-line no-undef
+    $('.menu-container').append(createMenuItem(dish));
+  }
+};
+
+const db = [{
+  image:"../images/pexels-dana.png", desc: "Beef chicken pork bacon chuck shortloin sirloin shank shoulder, meatloaf pastrami aute turkey proident eu t-bone consequat porkbelly, officia dolore flank est spareribs leberkas andouille. Sunt meatloaf officia occaecat esse veniam flank tri-tip pork, consectetur capicola mollit nostrud velit turkey consequat in, cow sed shankle est enim chicken tongue. Aute ", title: "Beef"
+},
+{
+  image:"../images/pexels-harry.jpeg", desc: "Beef chicken pork bacon chuck shortloin sirloin shank shoulder, meatloaf pastrami aute turkey proident eu t-bone consequat porkbelly, officia dolore flank est spareribs leberkas andouille. Sunt meatloaf officia occaecat esse veniam flank tri-tip pork, consectetur capicola mollit nostrud velit turkey consequat in, cow sed shankle est enim chicken tongue. Aute ", title: "Super shrimp"
+},
+{
+  image:"../images/pexels-josh.png", desc: "Beef chicken pork bacon chuck shortloin sirloin shank shoulder, meatloaf pastrami aute turkey proident eu t-bone consequat porkbelly, officia dolore flank est spareribs leberkas andouille. Sunt meatloaf officia occaecat esse veniam flank tri-tip pork, consectetur capicola mollit nostrud velit turkey consequat in, cow sed shankle est enim chicken tongue. Aute ", title: "Fresh's special pasta"
+},
+];
+
+
+////////////////////////////////////////////////////////////////////////////////////////////
+
+// eslint-disable-next-line no-undef
+$(document).ready(function() {
+  
+  /*
+  render dishes from the dummy db
+  to the home page. All elements on the homepage are also rendered here */
+  renderHomePage(db);
 
   ///////////////////////////////////////////////////////////////////////////////////////////////
   // Cart Page Implementation
@@ -137,13 +230,13 @@ $(document).ready(function () {
 
 
   // Remove Button
-  $(".remove button").click(function (event) {
+  $(".remove button").click(function(event) {
     event.preventDefault();
     // Remove dish from Dishes array
     //removeDish(dish_name, dishes);
 
     // Remove the current dish from cart page
-    const dish = $(this).closest(".dish-container")
+    const dish = $(this).closest(".dish-container");
 
     // update items count on the cart
     let $ordersLeft = $('.order-counter').text();
@@ -154,7 +247,7 @@ $(document).ready(function () {
   });
 
   // Add Button
-  $(".add").click(function (event) {
+  $(".add").click(function(event) {
     event.preventDefault();
     const output = $(this).closest(".dish-container").find('output');
     const MAX = 9;
@@ -173,7 +266,7 @@ $(document).ready(function () {
   });
 
   // Minus Button
-  $(".minus").click(function (event) {
+  $(".minus").click(function(event) {
     event.preventDefault();
     const output = $(this).closest(".dish-container").find('output');
     const Min = 1;
@@ -194,7 +287,7 @@ $(document).ready(function () {
   });
 
   // Checkout Button
-  $(".checkout button").click(function (event) {
+  $(".checkout button").click(function(event) {
     event.preventDefault();
     window.location.href = "/checkout";
   });
